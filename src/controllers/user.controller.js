4ef1fb661337e7c8ef5262800f0e0803f565512d
ThOMaZMe11o <thomazmello1912@gmail.com>
2024-10-11 09:56:45 -0300

    refactor config route create user and controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,11 +2,20 @@ const create = (req, res) => {
     const {name, username, email, password, avatar, background} = req.body;
 
     if (!name || !username || !email || !password || !avatar || !background) {
-
+        res.status(400).send({message: "Submit all fields for registration"});
     }
 
-    res.json (user)
-}
+    res.status(201).send({
+        message: "User created succesfully",
+        user: {
+            name,
+            username,
+            email,
+            avatar,
+            background 
+        }
+    });
+};
 
 
 module.exports = { create };
